8bf868d35ee4588de656742c2223a8569d7b584e
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-07-03 13:44:59 +0500

    Lecture 3
    JavaScript introduction.

---
+++ Lecture 3/nsrnb3.js
@@ -0,0 +1,24 @@
+let name = "Jahanzeb";
+let lastName = "Naeem";
+let a = 7;
+let b = 5;
+
+console.log(a+b);
+/* alert("Hi how are you?"); 
+hi */
+
+function sum(a, b) {
+ return a+b;   
+}
+
+console.log(sum(4, 5));
+
+// const car = {type:"Honda", model:"City", color:"Gray"};
+
+console.log(car.model);
+
+let text = "We are the so-called \"Vikings\" from the north.";
+
+// console.log(text.slice(1));
+console.log(text.split(" "));
+
