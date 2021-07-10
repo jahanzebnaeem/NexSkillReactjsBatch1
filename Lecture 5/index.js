86d5d5e71ea25da66b12e0acedc4ced01b9320f8
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-07-10 16:29:52 +0500

    Lecture 5 Final
    Final code after class completion.

--- Lecture 5/index.js
+++ Lecture 5/index.js
@@ -1,4 +1,16 @@
-const superheroes = require('superheroes');
+const express = require('express');
 
-// console.log(superheroes.all);
-console.log(superheroes.random());
+const app = express();
+
+app.get('/', function (req, res) {
+  // console.log(request);
+  res.send("Welcome");
+});
+
+app.get('/contact', function (req, res) {
+  res.send("This is the contact page.");
+});
+
+app.listen(3000, function() {
+  console.log("Server listening on port 3000");
+});
