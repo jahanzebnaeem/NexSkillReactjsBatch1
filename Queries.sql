7a80ede9f707ba339534c6df6e143d31e497ad6d
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-07-27 07:13:50 +0500

    Lecture 8
    SQL vs NoSQL + Understanding SQL + SQL Queries.

---
+++ Queries.sql
@@ -0,0 +1,15 @@
+/* CREATE TABLE Orders (
+    OrderID int NOT NULL,
+    CustomerID int,
+  	ProductID int,
+    PRIMARY KEY (OrderID),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(id),
+  	FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+); */
+
+--INSERT INTO Customers VALUES (1, 'Naeem', 'Jahanzeb', 23);
+--SELECT * from Customers;
+--INSERT INTO Orders (1, 1, 1);
+SELECT Customers.FirstName 
+FROM Orders INNER JOIN Customers
+ON
