c05c0761b288cfca342a00448717613d230de33c
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-08-02 22:50:29 +0500

    Lecture 11
    Creating an API.

---
+++ Wikiapp/app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).catch(function(err) {
+  console.log(err);
+});
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function(req, res) {
+  Article.find(function(err, foundArticles) {
+    if (!err) {
+      // console.log(foundArticles);
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  })
+});
+
+app.post("/articles", function(req, res) {
+  // console.log(req.body.title);
+  // console.log(req.body.content);
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+});
+
+app.listen(4000, function() {
+  console.log("Server started on port 4000");
+});
